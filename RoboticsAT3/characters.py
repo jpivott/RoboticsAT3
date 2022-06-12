cf5fa57876b5be1d3f1a33d2c3bec055297ee205
Josh Pivott <63906307+jpivott@users.noreply.github.com>
2022-06-12 10:11:35 +0800

    Created base for a merchant character

--- RoboticsAT3/characters.py
+++ RoboticsAT3/characters.py
@@ -1,1 +1,11 @@
+class Characters():
+    def __init__(self):
+        pass
 
+    def __str__(self):
+        return self.description
+
+
+class Merchant(Characters):
+    def __init__(self):
+        self.description = "Merchant"
